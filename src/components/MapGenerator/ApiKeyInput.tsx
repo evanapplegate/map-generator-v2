e9caa96f687eea031f11444ef6f6eb593f998e3d
Evan Applegate <evanapplegate@users.noreply.github.com>
2025-02-06 18:35:49 -0800

    removed API key, whoops

--- src/components/MapGenerator/ApiKeyInput.tsx
+++ src/components/MapGenerator/ApiKeyInput.tsx
@@ -6,7 +6,7 @@ interface ApiKeyInputProps {
 }
 
 const ApiKeyInput = ({ onApiKeyChange }: ApiKeyInputProps) => {
-  const [apiKey, setApiKey] = useState('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');
+  const [apiKey, setApiKey] = useState('fake-key');
 
   useEffect(() => {
     // Notify parent of initial value
